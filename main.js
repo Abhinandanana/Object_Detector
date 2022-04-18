b46fed2940a9bb143b5684ab8598eefab0f9bae6
Abhinandanana <83591578+Abhinandanana@users.noreply.github.com>
2022-04-18 19:15:03 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+function back_btn1(){
+    window.location= "index.html"
+}
+function back_btn2(){
+    window.location= "index.html"
+}
+function back_btn3(){
+    window.location= "index.html"
+}
+function back_btn4(){
+    window.location= "index.html"
+}
+function back_btn5(){
+    window.location= "index.html"
+}
+
+function bed_room(){
+    window.location= "bed_room.html"
+}
+function TV_AC(){
+    window.location= "TV_&_AC.html"
+}
+function desk(){
+    window.location= "desk.html"
+}
+function bottles(){
+    window.location= "bottles.html"
+}
+function fruit_basket(){
+    window.location= "fruit_basket.html"
+}
